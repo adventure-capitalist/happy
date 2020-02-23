b493d42b7417664eabcbdceade0a11a40bf80a14
Agata <agata@swistowski.org>
2020-02-23 17:02:54 +0000

    make it work

--- client/src/components/columns.jsx
+++ client/src/components/columns.jsx
@@ -1,55 +1,65 @@
 import React, { Component } from "react";
 import axios from "axios";
-let arrhappy = [];
-let arrsad = [];
-const api = "http://localhost:3001/sites";
 
 class Columns extends Component {
   state = {
-    data: [{ id: 0, name: "", urls: ["", ""] }],
     happy: [],
-    sad: [],
-    faking: []
+    sad: [], // not a state
+    faking: [] // not a state
   };
   componentDidMount() {
     this.getDataFromDb();
-    this.parseData(this.state.data);
+    // this.parseData(this.state.data);  - data will be updated after request
   }
 
   getDataFromDb = () => {
-    axios.get(api).then(result => {
-      console.log(result.data);
-      let variable = [];
-      variable = result.data;
-      this.setState({ data: variable });
+    axios.get("/sites/check").then(result => {
+      console.log("data", result.data, "done");
+      this.checkStatus(result.data);
+      // this.setState({ data: variable });
     });
   };
 
-  parseData = data => {
-    data.map(site => {
-      this.checkStatus(site);
-    });
-  };
+  // parseData = data => {
+  //   data.map(site => {
+  //     this.checkStatus(site);
+  //   });
+  // };
 
-  checkStatus(site) {
-    site.urls.map(url =>
-      axios.get(url).then(response => {
-        console.log(response);
-        if (response.status !== 200) {
-          arrsad.push(site.name);
-          console.log(site);
-        } else {
-          arrhappy.push(site.name);
-          console.log(site);
-        }
-      })
-    );
-    this.update();
+  checkStatus(status) {
+    const result = Object.keys(status).map(key => {
+      const statuses = new Set(Object.values(status[key]));
+      // is happy if it's always 200, sad if there is no 200, faking if it's mix
+      const state = statuses.has(200)?(statuses.size===1?"happy":"faking"):"sad";
+      return [key, state]
+    }).reduce((acc, [name, state]) => {
+      acc[state].push(name);
+      return acc;
+    }, {happy: [], sad: [], faking: []});
+    this.setState(result);
+
+    // not sure what "app is happy mean - all urls are correct,
+
+    // it's not possible to check status code of site from react (CORS)
+    // site.urls.map(url =>
+    //   axios.get(url).then(response => {
+    //     console.log(`get: ${url}:`, response);
+    //     if (response.status !== 200) {
+    //       arrsad.push(site.name);
+    //       console.log(site);
+    //     } else {
+    //       arrhappy.push(site.name);
+    //       console.log(site);
+    //     }
+    //   })
+    // );
+
+    ///this.update();
   }
 
-  update() {
-    this.setState({ happy: arrhappy });
-  }
+  // update() {
+  //   this.setState({ happy: arrhappy });
+  // }
 
   render() {
     return (
--- node-api-postgres/index.js
+++ node-api-postgres/index.js
@@ -23,3 +23,5 @@ app.get("/sites/check", db.checkSites);
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
 });
+
+app.get("checker")
